09bc5f697629cb896f9c435b41102fcccc55c4e7
hahah <461041392@qq.com>
2021-11-09 16:18:43 +0800

    Update alias.py

--- ge/alias.py
+++ ge/alias.py
@@ -9,8 +9,8 @@ def create_alias_table(area_ratio):
     """
     l = len(area_ratio)
     accept, alias = [0] * l, [0] * l
-    small, large = [], []
-    area_ratio_ = np.array(area_ratio) * l
+    small, large = [], [] #存id/索引
+    area_ratio_ = np.array(area_ratio) * l #数组创建
     for i, prob in enumerate(area_ratio_):
         if prob < 1.0:
             small.append(i)
@@ -18,11 +18,11 @@ def create_alias_table(area_ratio):
             large.append(i)
 
     while small and large:
-        small_idx, large_idx = small.pop(), large.pop()
-        accept[small_idx] = area_ratio_[small_idx]
-        alias[small_idx] = large_idx
+        small_idx, large_idx = small.pop(), large.pop() #pop()用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值。
+        accept[small_idx] = area_ratio_[small_idx] #索引为small_idx对应的小于1的面积
+        alias[small_idx] = large_idx #索引为small_idx对应的大于1的索引
         area_ratio_[large_idx] = area_ratio_[large_idx] - \
-            (1 - area_ratio_[small_idx])
+            (1 - area_ratio_[small_idx]) #剩余面积
         if area_ratio_[large_idx] < 1.0:
             small.append(large_idx)
         else:
@@ -46,8 +46,8 @@ def alias_sample(accept, alias):
     :return: sample index
     """
     N = len(accept)
-    i = int(np.random.random()*N)
-    r = np.random.random()
+    i = int(np.random.random()*N) #产生1-N之间的随机数i，决定落在哪一列
+    r = np.random.random() #产生0-1之间的随机数
     if r < accept[i]:
         return i
     else:
